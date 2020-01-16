fb28a22f9176fbd7d251f327f1a82c55d6fbf627
Vodyanov Dmitriy <dmitriy.vodyanov@tortugasocial.com>
2020-01-16 18:03:20 +0300

    feature(content_main): setting game range

--- app/src/main/java/com/example/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/guessinggame/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.guessinggame;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +12,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textClickGuess;
     private int theNumber;
     private int numberOfTries = 0;
+    private int range = 100;
+    private TextView labelRange;
 
 
     public void checkGuess() {
@@ -58,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         } catch (Exception e) {
-            message = "Enter a whole number between 1 and 100.";
+            message = "Enter a whole number between 1 and " + range + ".";
         }
         finally {
             textClickGuess.setText(message);
@@ -70,6 +75,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void newGame() {
         theNumber = (int)(Math.random() * 100 + 1);
+        labelRange.setText("Enter a number between 1 and " + range + ".");
+        editTextNumber.requestFocus();
+        editTextNumber.selectAll();
+
     }
 
     @Override
@@ -80,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
         editTextNumber = findViewById(R.id.editTextNumber);
         buttonGuess = findViewById(R.id.buttonGuess);
         textClickGuess = findViewById(R.id.textClickGuess);
+        labelRange = findViewById(R.id.textView2);
 
         newGame();
 
@@ -114,22 +124,63 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public boolean onOptionsItemSelected(final MenuItem item) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                final CharSequence [] items = {"1 to 10", "1 to 100", "1 to 1000"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Select the Range: ");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int item) {
+                        switch (item) {
+                            case 0:
+                                range = 10;
+                                newGame();
+                                break;
+                            case 1:
+                                range = 100;
+                                newGame();
+                                break;
+                            case 2:
+                                range = 1000;
+                                newGame();
+                                break;
+                        }
+                        dialogInterface.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                return true;
+            case R.id.action_newgame:
+                newGame();
+                return true;
+            case R.id.action_gamestats:
+                return true;
+            case R.id.action_about:
+                AlertDialog aboutDialog = new AlertDialog.Builder(MainActivity.this).create();
+                aboutDialog.setTitle("About Guessing Game");
+                aboutDialog.setMessage("(c)2018 Your Name.");
+                aboutDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                aboutDialog.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
